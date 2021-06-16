21015b15abfdf2b15a784f430add72a3320a485f
Håkon Antonsen <haakon@antons1.net>
2021-06-16 10:18:18 +0200

    Move apollo to app, add routes

--- App.jsx
+++ App.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import {
-    useQuery,
-    gql
-} from '@apollo/client';
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider
+} from "@apollo/client";
+import { HashRouter as Router, Switch, Route} from 'react-router-dom';
 
-const EPISODES = gql`
-query {
-    episodes {
-        results {
-            name
-            air_date
-            episode
-        }
-    }
-}
-`
+import { Episodes } from './components/Episodes';
+import { Episode } from './components/Episode';
+import { Character } from './components/Character';
+
+const client = new ApolloClient({
+    uri: 'https://rickandmortyapi.com/graphql',
+    cache: new InMemoryCache()
+});
 
 export const App = () => {
-    const { loading, error, data } = useQuery(EPISODES);
     return (
-        <div>
-            <h1>Episoder</h1>
-            <div>
-            {loading && "Laster..."}
-            {error && <span>Det skjedde en feil: <pre>{JSON.stringify(error, null, 2)}</pre></span>}
-            {!loading && !error && data && <ul>{data.episodes.results.map(({ name, air_date, episode }, idx) => <li key={idx}>{name} - {episode} - {air_date}</li>)}</ul>}
-            </div>
-        </div>
+        <ApolloProvider client={client}>
+            <Router>
+                <Switch>
+                    <Route path="/characters/:id" component={Character} />
+                    <Route path="/episodes/:id" component={Episode}/>
+                    <Route path="/episodes" component={Episodes} />
+                    <Route exact path="/" component={Episodes} />
+                </Switch>
+            </Router>
+        </ApolloProvider>
     )
 }
--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider
-} from "@apollo/client";
 
 import { App } from './App';
 
-const client = new ApolloClient({
-    uri: 'https://rickandmortyapi.com/graphql',
-    cache: new InMemoryCache()
-});
-
-ReactDOM.render(<ApolloProvider client={client}>
-    <App />
-</ApolloProvider>, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
